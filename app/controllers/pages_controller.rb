5a20fa863c1cbefadc570a3880f144f0d7c61b40
Matei <matei.david.35@gmail.com>
2016-04-25 14:08:57 +0000

    Finished styling on explore page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,20 +7,24 @@ class PagesController < ApplicationController
   @posts = Post.all
   @newPost = Post.new
   end
-  
   def profile
+  
     if (User.find_by_username(params[:id]))
       @username = params[:id]
-    else
-      redirect_to root_path, :notice=> "User not found!"
+    else 
+      redirect_to root_path, :notice=> "User not found!" 
     end
-  
+    
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
+    
+    @toFollow = User.all.last(5)
   end
   
   def explore
     @posts = Post.all
+    @newPost = Post.new
+    @toFollow = User.all.last(5)
   end
   
 
